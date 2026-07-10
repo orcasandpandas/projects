a457b48b19ce655de7f3440f465b6ae73c87bc5a
orcasandpandas <149914308+orcasandpandas@users.noreply.github.com>
2026-07-10 09:29:49 -0700

    Made database and insert more functional

--- project_1/src/database.py
+++ project_1/src/database.py
@@ -3,15 +3,15 @@ import psycopg
 import pandas as pd
 import os
 
-DB_PARAMS = "user=postgres password=pass host=localhost"
-
+DB_PARAMS = "dbname=weather user=postgres password=pass host=localhost"
 def get_sql_queries():
-    sql_file_path = "/sql/insert.sql"
+    # This safely builds the path relative to where database.py is located
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    sql_file_path = os.path.join(current_dir, '..', 'sql', 'insert.sql')
+    
     with open(sql_file_path, 'r') as file:
         sql_content = file.read()
         
-    # We split the file based on the SQL comments to get the exact query strings.
-    # WARNING: Do not change the comments in your insert.sql file or this will break!
     try:
         city_query = sql_content.split('-- INSERT_CITY')[1].split('-- INSERT_WEATHER')[0].strip()
         weather_query = sql_content.split('-- INSERT_WEATHER')[1].strip()
@@ -22,8 +22,7 @@ def get_sql_queries():
 
 def load_data_to_db(df):
     print("Connecting to the database...")
-    city_query, weather_query = get_sql_queries()
-    
+    city_query, weather_query = get_sql_queries()    
     # We use psycopg to connect to the database
     with psycopg.connect(DB_PARAMS) as conn:
         with conn.cursor() as cur:
