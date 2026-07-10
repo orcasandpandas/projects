22179dc414bc020df48892553f9338b5065cc69a
orcasandpandas <149914308+orcasandpandas@users.noreply.github.com>
2026-07-10 07:59:00 -0700

    Modifications!

--- project_1/src/api.py
+++ project_1/src/api.py
@@ -1,24 +1,37 @@
+# src/api.py
+import httpx
+import json
+
 URL = "https://archive-api.open-meteo.com/v1/archive"
 
-import httpx
+CITIES = [
+    {"name": "Chicago", "lat": 41.88, "lon": 	-87.62},
+    {"name": "Phoenix", "lat": 33.44, "lon": -112.07},
+    {"name": "Beijing", "lat": 39.92, "lon": 116.38}
+]
 
-
-
-async def get_weather():
-    params = {
-	"latitude": 52.52,
-	"longitude": 13.41,
-	"start_date": "2026-06-13",
-	"end_date": "2026-06-27",
-	"hourly": ["temperature_2m",
-        "precipitation",
-        "wind_speed_10m"]
-    }
+def get_weather():
+    all_city_data = []
     
     with httpx.Client() as client:
-        response = client.get(URL, params=params)
-        response.raise_for_status()
+        for city in CITIES:
+            params = {
+                "latitude": city["lat"],
+                "longitude": city["lon"],
+                "start_date": "2023-01-01", # Pick your date range
+                "end_date": "2023-12-31",
+                "daily": ["temperature_2m_max", "precipitation_sum", "wind_speed_10m_max"],
+                "timezone": "auto"
+            }
+            
+            response = client.get(URL, params=params)
+            response.raise_for_status()
+            
+            data = response.json()
+            data['city_name'] = city["name"]
+            all_city_data.append(data)
 
-    data = response.json()
-
-    return data
+    with open("raw_weather_data.json", "w") as f:
+        json.dump(all_city_data, f, indent=4)
+        
+    return all_city_data
